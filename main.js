bd4009da7d7d8133775849a4c0580fc08fab7769
Peggy Carambas <PCarambas@gmail.com>
2022-10-07 10:47:10 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const i of clay) {
+ const mug = `${i} coffee mug`
+ toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
